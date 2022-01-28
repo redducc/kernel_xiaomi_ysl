df928839ae89042f6dfb24071e1de9e532765776
Dakkshesh <dakkshesh5@gmail.com>
2022-01-28 23:43:07 +0530

    Kprofiles: Simplify Auto Kprofiles
    Avoid the set_mode mess, and directly make active_mode() check for screen_on
    
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- drivers/misc/kprofiles.c
+++ drivers/misc/kprofiles.c
@@ -16,14 +16,13 @@
 #if defined(CONFIG_AUTO_KPROFILES_MSM_DRM) || defined(CONFIG_AUTO_KPROFILES_FB)
 static bool screen_on = true;
 #endif
+
 static unsigned int mode = 0;
-#if defined(CONFIG_AUTO_KPROFILES_MSM_DRM) || defined(CONFIG_AUTO_KPROFILES_FB)
-static unsigned int set_mode;
-#endif
-static unsigned int rollback_mode;
 static bool auto_kprofiles = true;
-module_param(auto_kprofiles, bool, 0664);
 module_param(mode, uint, 0664);
+module_param(auto_kprofiles, bool, 0664);
+
+static unsigned int rollback_mode;
 
 inline void kprofiles_set_mode_rollback(unsigned int level, unsigned int duration_ms)
 {
@@ -92,14 +91,6 @@ static inline int common_notifier_callback(struct notifier_block *self,
 	}
 #endif
 
-	if (!screen_on && auto_kprofiles) {
-		set_mode = mode;
-		kprofiles_set_mode(1);
-	}
-	else if(screen_on && auto_kprofiles) {
-		kprofiles_set_mode(set_mode);
-	}
-
 out:
 	return NOTIFY_OK;
 }
@@ -107,6 +98,11 @@ out:
 
 inline unsigned int active_mode(void)
 {
+#if defined(CONFIG_AUTO_KPROFILES_MSM_DRM) || defined(CONFIG_AUTO_KPROFILES_FB)
+	if (!screen_on)
+		return 1;
+#endif
+
 	if (mode < 4)
 		return mode;
 
@@ -122,13 +118,10 @@ static struct notifier_block common_notifier_block = {
 
 static int  __init kprofiles_init(void)
 {
-#if defined(CONFIG_AUTO_KPROFILES_MSM_DRM) || defined(CONFIG_AUTO_KPROFILES_FB)
-	set_mode = mode;
 #ifdef CONFIG_AUTO_KPROFILES_MSM_DRM
 	msm_drm_register_client(&common_notifier_block);
 #elif defined(CONFIG_AUTO_KPROFILES_FB)
 	fb_register_client(&common_notifier_block);
-#endif
 #endif
 	return 0;
 }
